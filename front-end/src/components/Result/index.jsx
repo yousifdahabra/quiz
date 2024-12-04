53d2c08497ca5e0fca8236e52dc8c2d8c0d8a04a
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-04 21:15:46 +0200

    Front-end - update  components\Result add score, totalQuestions

--- front-end/src/components/Result/index.jsx
+++ front-end/src/components/Result/index.jsx
@@ -2,5 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 const Result = () => {
+    const score = useSelector((state) => state.quiz.score);
+    const totalQuestions = useSelector((state) => state.quiz.currentQuiz?.questions.length);
+    return (
+        <div className="result-page">
+          <h1>Quiz Result</h1>
+          <p>
+            You scored {score} out of {totalQuestions}
+          </p>
+          <Link to="/">Back to Quiz List</Link>
+        </div>
+      );
+    
+    
 };
 export default Result;
