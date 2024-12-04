e57ee95b9a4d1e27a6805fe2152898d4d282408d
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-04 21:10:39 +0200

    Front-end - update components\Question add question options

--- front-end/src/components/Question/index.jsx
+++ front-end/src/components/Question/index.jsx
@@ -5,10 +5,29 @@ const Question = ({ question, userAnswer, onAnswer }) => {
 
     return (
         <div className="question">
-            <p>{questionTitle}</p>
-
-        </div>
-    );
+        <p>{questionTitle}</p>
+        {type === 'options' ? (
+          options.map((opt, index) => (
+            <div key={index}>
+              <input
+                type="radio"
+                name={question.id}
+                value={opt}
+                checked={userAnswer === opt}
+                onChange={() => onAnswer(opt)}
+              />
+              <label>{opt}</label>
+            </div>
+          ))
+        ) : (
+          <input
+            type="text"
+            value={userAnswer || ''}
+            onChange={(e) => onAnswer(e.target.value)}
+          />
+        )}
+      </div>
+      );
 };
 
 export default Question;
