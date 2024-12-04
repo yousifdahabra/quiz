1837bd34cf2e72da653f6e1f64b7b3f5fbc26f89
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-04 18:32:45 +0200

    Front-end - update  components\QuizList add  setQuizzes from redux/quizSlice

--- front-end/src/components/QuizList/index.jsx
+++ front-end/src/components/QuizList/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from 'react-router-dom';
- 
+import { setQuizzes } from '../../redux/quizSlice';
+
 
 const QuizList = () => {
 
