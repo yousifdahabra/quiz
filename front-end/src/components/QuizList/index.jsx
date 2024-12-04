8f8157c190deaa1f135c03c6c2904d53f0055319
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-04 18:33:28 +0200

    Front-end - update  components\QuizList add  data/quizzes.json

--- front-end/src/components/QuizList/index.jsx
+++ front-end/src/components/QuizList/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from 'react-router-dom';
 import { setQuizzes } from '../../redux/quizSlice';
+import quizzesData from '../../data/quizzes.json';
 
 
 const QuizList = () => {
