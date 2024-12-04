7a0796e9468fb27e3b0e9da450b05d9b60512ed2
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-04 21:19:32 +0200

    Front-end - update  components\Result

--- front-end/src/components/Result/index.jsx
+++ front-end/src/components/Result/index.jsx
@@ -6,14 +6,14 @@ const Result = () => {
     const totalQuestions = useSelector((state) => state.quiz.currentQuiz?.questions.length);
     return (
         <div className="result-page">
-          <h1>Quiz Result</h1>
-          <p>
+        <h1>Quiz Result</h1>
+        <p>
             You scored {score} out of {totalQuestions}
-          </p>
-          <Link to="/">Back to Quiz List</Link>
+        </p>
+        <Link to="/">Back to Quiz List</Link>
         </div>
-      );
-    
+    );
+
     
 };
 export default Result;
