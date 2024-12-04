1232a29e259aaef7a7d0a8e510c0b2a6616b2e5b
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-04 21:11:10 +0200

    Front-end - update pages\QuizPage add question  components

--- front-end/src/pages/QuizPage/index.jsx
+++ front-end/src/pages/QuizPage/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { startQuiz, submitAnswer, calculateScore } from '../../redux/quizSlice';
+import Question from './../../components/Question/';
 
 const QuizPage = () => {
 }
