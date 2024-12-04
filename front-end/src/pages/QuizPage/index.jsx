3067b5bd6966d862686627cc1e9cf9cf1bca56aa
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-04 18:49:43 +0200

    Front-end - update pages\QuizPage  add redux ,react

--- front-end/src/pages/QuizPage/index.jsx
+++ front-end/src/pages/QuizPage/index.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { startQuiz, submitAnswer, calculateScore } from '../../redux/quizSlice';
+
 const QuizPage = () => {
 }
+
 export default QuizPage;
