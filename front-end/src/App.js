813282bd490f43e7b9976f0c3aeaf31a8571b782
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-04 21:25:46 +0200

    Front-end - update App.js  update path  QuizList,  QuizPage, Result

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,20 @@
 import './ui/styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import QuizList from './components/QuizList';
-import QuizPage from './components/QuizPage';
+import QuizPage from './pages/QuizPage';
 import Result from './components/Result';
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<QuizList />} />
+        <Route path="/quiz/:quizId" element={<QuizPage />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </Router>
+
+);
 }
 
 export default App;
