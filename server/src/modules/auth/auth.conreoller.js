ba9ed0898e17b3762525a36b420a08ca525d9e15
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-06 18:44:32 +0200

    Back-end - update auth.conreoller add hashed

--- server/src/modules/auth/auth.conreoller.js
+++ server/src/modules/auth/auth.conreoller.js
@@ -1,6 +1,14 @@
+import bcrypt from "bcrypt"
+
 export const login = async (req,res) => {
 
 }
 export const register = async (req,res) => {
-    const {username,password} = req.body
+    const {username,password} = req.body;
+
+    try {
+        const hashed = await bcrypt.hash(password,10)
+    } catch (error) {
+        
+    }
 }
