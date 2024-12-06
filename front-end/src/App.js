3dcc200925549e821da58d72ca74991ddd504e6b
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-06 22:41:23 +0200

    Front-end - add login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,14 +6,16 @@ import QuizPage from './pages/QuizPage';
 import Result from './components/Result';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import Login from './pages/login';
 function App() {
   return (
     <Provider store={store}>
 
     <Router>
       <Routes>
-        <Route path="/" element={<QuizList />} />
-        <Route path="/quiz/:quizId" element={<QuizPage />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/quiz_list" element={<QuizList />} />
+      <Route path="/quiz/:quizId" element={<QuizPage />} />
         <Route path="/result" element={<Result />} />
       </Routes>
     </Router>
---
+++ front-end/src/pages/login/index.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { SignInPage } from '@toolpad/core/SignInPage';
+import { useTheme } from '@mui/material/styles';
+
+const providers = [{ id: 'credentials', name: 'Email and Password' }];
+
+const signIn = async (provider, formData) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      alert(
+        `Signing in with "${provider.name}" and credentials: ${formData.get('email')}, ${formData.get('password')}`,
+      );
+      resolve();
+    }, 300);
+  });
+  return promise;
+};
+
+export default function CredentialsSignInPage() {
+  const theme = useTheme();
+  return (
+    <AppProvider theme={theme}>
+      <SignInPage signIn={signIn} providers={providers} />
+    </AppProvider>
+  );
+}
