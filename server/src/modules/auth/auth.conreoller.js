4ac07132593bedabe0a2bbb3d6123540eb30c43c
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-06 20:47:17 +0200

    Back-end - update  auth.conreoller.js update login with JWT

--- server/src/modules/auth/auth.conreoller.js
+++ server/src/modules/auth/auth.conreoller.js
@@ -1,7 +1,39 @@
 import bcrypt from "bcrypt"
 import { User } from "../../db/models/user.model.js";
+import jwt from "jsonwebtoken"
 
 export const login = async (req,res) => {
+    const {username,password} = req.body;
+
+    try {
+        const user = await User.findOne({username}).select("+password")
+        if(!user){
+            return res.status(400).send({
+                message:"Invalid credntials"
+            })
+        }
+
+        const cheack = await bcrypt.compare(password,user.password)
+
+        if(!cheack){
+            return res.status(404).send({
+                message:"Invalid credntials"
+            })
+        }
+        const token = await jwt.sign({id:user.id},process.env.JWT_SECRET)
+        return res.status(201).send({
+            message:"login user",
+            user,
+            token
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({
+            message:"something wan't wrong"
+        })
+
+    }
+    
 
 }
 export const register = async (req,res) => {
