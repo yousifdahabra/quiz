a00eb5b2182ec7cba96b93bcc52a16f835682eee
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-06 19:56:59 +0200

    Back-end - update auth.routes add registration

--- server/src/modules/auth/auth.routes.js
+++ server/src/modules/auth/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login } from "./auth.conreoller.js";
+import { login, register } from "./auth.conreoller.js";
 
 const authRouter = new Router();
 
 authRouter.post("/login",login)
+authRouter.post("/registration",register)
 
 
 export default authRouter
