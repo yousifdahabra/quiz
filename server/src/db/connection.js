20cd2d6e7e51fb23ea1f9bfa76ab4a9ab61ab0c3
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-06 17:44:43 +0200

    Back-end - Update index add app core

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,6 +1,13 @@
 import { connect} from "mongoose";
 
 const connectToDatabase = async () => {
- };
+  try {
+    await connect("mongodb://localhost:27017/quizzes");
+
+    console.log("Connected to database");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default connectToDatabase;
